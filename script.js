2e996b6e5290e47cc78f8891da75f713a8e5a94b
Nelson Lamounier <lamounierleao@gmail.com>
2024-02-16 19:54:12 +0000

    last update

--- script.js
+++ script.js
@@ -184,102 +184,107 @@ function hideElement() {
 
 /* Sign up/ Sign in Section */
 
-const sectionSignUp = document.querySelector(".section-signup");
-const headingSpan2 = document.querySelector(".heading-span-2");
-const form = document.querySelector(".form");
+function signUpIn() {
+  const sectionSignUp = document.querySelector(".section-signup");
+  const headingSpan2 = document.querySelector(".heading-span-2");
+  const form = document.querySelector(".form");
 
-const clearForm = () => {
-  document.querySelectorAll(".form-input-wrapper").forEach((item) => {
-    item.className = "form-input-wrapper";
+  const clearForm = () => {
+    document.querySelectorAll(".form-input-wrapper").forEach((item) => {
+      item.className = "form-input-wrapper";
+    });
+    form.reset();
+  };
+
+  document.querySelector(".signup-btn").addEventListener("click", () => {
+    sectionSignUp.classList.add("change-sign");
+    setTimeout(() => {
+      headingSpan2.textContent = "Up";
+    }, 200);
+    form.className = "form sign-up";
+    clearForm();
   });
-  form.reset();
-};
 
-document.querySelector(".signup-btn").addEventListener("click", () => {
-  sectionSignUp.classList.add("change-sign");
-  setTimeout(() => {
-    headingSpan2.textContent = "Up";
-  }, 200);
-  form.className = "form sign-up";
-  clearForm();
-});
+  document.querySelector(".signin-btn").addEventListener("click", () => {
+    sectionSignUp.classList.remove("change-sign");
+    setTimeout(() => {
+      headingSpan2.textContent = "In";
+    }, 200);
+    form.className = "form sign-in";
+    clearForm();
+  });
 
-document.querySelector(".signin-btn").addEventListener("click", () => {
-  sectionSignUp.classList.remove("change-sign");
-  setTimeout(() => {
-    headingSpan2.textContent = "In";
-  }, 200);
-  form.className = "form sign-in";
-  clearForm();
-});
+  const username = document.getElementById("username");
+  const email = document.getElementById("email");
+  const password = document.getElementById("password");
+  const password2 = document.getElementById("password2");
 
-const username = document.getElementById("username");
-const email = document.getElementById("email");
-const password = document.getElementById("password");
-const password2 = document.getElementById("password2");
+  const error = (input, message) => {
+    const inputWrapper = input.parentElement;
+    inputWrapper.className = "form-input-wrapper error";
+    inputWrapper.querySelector(".message").textContent = message;
+  };
 
-const error = (input, message) => {
-  const inputWrapper = input.parentElement;
-  inputWrapper.className = "form-input-wrapper error";
-  inputWrapper.querySelector(".message").textContent = message;
-};
+  const success = (input) => {
+    const inputWrapper = input.parentElement;
+    inputWrapper.className = "form-input-wrapper success";
+  };
 
-const success = (input) => {
-  const inputWrapper = input.parentElement;
-  inputWrapper.className = "form-input-wrapper success";
-};
+  const checkEmail = (input) => {
+    const regEx =
+      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-const checkEmail = (input) => {
-  const regEx =
-    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (regEx.test(input.value.trim())) {
+      success(input);
+    } else {
+      error(input, "Email is not valid");
+    }
+  };
 
-  if (regEx.test(input.value.trim())) {
-    success(input);
-  } else {
-    error(input, "Email is not valid");
-  }
-};
-
-const checkRequiredFields = (inputArr) => {
-  inputArr.forEach((input) => {
-    if (input.value.trim() == "") {
-      if (input.id === "password2") {
-        error(input, "password confirmation is required");
+  const checkRequiredFields = (inputArr) => {
+    inputArr.forEach((input) => {
+      if (input.value.trim() == "") {
+        if (input.id === "password2") {
+          error(input, "password confirmation is required");
+        } else {
+          error(input, `${input.id} is required`);
+        }
       } else {
-        error(input, "${input.id} is required");
+        success(input);
       }
+    });
+  };
+
+  const checkLength = (input, min, max) => {
+    if (input.value.length < min) {
+      error(input, `${input.id} must be at least ${min} characters`);
+    } else if (input.value.lenght > max) {
+      error(input, `${input.id} must be less than ${max} characters`);
     } else {
       success(input);
     }
+  };
+
+  const passwordsMatch = (input1, input2) => {
+    if (input1.value !== input2.value) {
+      error(input2, "Passwords do not match");
+    }
+  };
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    if (form.classList[1] === "sign-up") {
+      checkRequiredFields([username, email, password, password2]);
+      checkLength(username, 2, 15);
+      checkLength(password, 5, 25);
+      passwordsMatch(password, password2);
+    } else {
+      checkRequiredFields([email, password]);
+    }
+    checkEmail(email);
   });
-};
+}
 
-const checkLength = (input, min, max) => {
-  if (input.value.length < min) {
-    error(input, "${input.id} must be at least ${min} characters");
-  } else if (input.value.lenght > max) {
-    error(input, "${input.id} must be less than ${max} characters");
-  } else {
-    success(input);
-  }
-};
-
-const passwordsMatch = (input1, input2) => {
-  if (input1.value !== input2.value) {
-    error(input2, "Passwords do not match");
-  }
-};
-
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-
-  if (form.classList[1] == "sign-up") {
-    checkRequiredFields([username, email, password, password2]);
-    checkLength(username, 2, 15);
-    checkLength(password, 5, 25);
-    passwordsMatch(password, password2);
-  } else {
-    checkRequiredFields([email, password]);
-  }
-  checkEmail(email);
-});
+/* End of Sign up/ Sign in Section */
+/* Payment Section */
